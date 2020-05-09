39f55c149b1cf859313e8539b4e67aa33a3e3ad3
Sharath7 <sharathpavan7@gmail.com>
2020-05-10 01:16:44 +0530

    v0.0.1 - loading fragments in activity.

--- app/src/main/java/com/sharath/fragmentslifecycle/MainActivity.java
+++ app/src/main/java/com/sharath/fragmentslifecycle/MainActivity.java
@@ -1,14 +1,34 @@
 package com.sharath.fragmentslifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frag_container);
+        loadFragment();
+    }
+
+    private void loadFragment() {
+        FragmentA fa = new FragmentA();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, fa).addToBackStack(null).commit();
+
+    }
+
+    public void onClick(View view) {
+        FragmentB fb = new FragmentB();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, fb).addToBackStack(null).commit();
     }
 }
